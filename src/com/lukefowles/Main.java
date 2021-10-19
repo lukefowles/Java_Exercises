95d98507b979e2c7f759e6aa0a791ec7cd67259f
Luke Fowles <luke.fowles15@gmail.com>
2021-10-19 17:20:50 +0100

    day 2 exercises included

--- src/com/lukefowles/Main.java
+++ src/com/lukefowles/Main.java
@@ -1,9 +1,73 @@
 package com.lukefowles;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.List;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+     totalDecimalString();
+     longestStringMethod(new String[]{"hello", "bingo", "ola", "bye", "ciao"});
+     reverseString();
+     tellJoke();
+
+    }
+
+    static void tellJoke() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        do {
+            System.out.println("Would you like to hear a joke? [yes/no]");
+            userInput = scanner.nextLine();
+            if (userInput.compareTo("yes") == 0) {
+                System.out.println("Luke's knowledge of Java :)");
+            }
+        }
+        while (userInput.compareTo("yes") == 0);
+    }
+
+    static void reverseString() {
+        String [] hello = {"you", "are", "how", "Hello"};
+        String [] message = new String[4];
+
+        for(int i = 0; i < hello.length; i++) {
+            message[i] = hello[hello.length - 1 - i];
+            System.out.print(message[i] + " ");
+        }
+
+    }
+
+
+    static void totalDecimalString() {
+        String numbers = "0.90, 1.00, 9.00, 8.78, 0.01";
+        String [] numbersArray = numbers.split(", ");
+        double total = 0.0;
+        for (String number: numbersArray){
+            double numberParsed = Double.parseDouble(number);
+            total += numberParsed;
+        }
+        BigDecimal result = BigDecimal.valueOf(total);
+        System.out.println(result);
+
+    }
+
+    static void longestStringMethod(String [] words) {
+        int letterCount = 0;
+        for (String word: words){
+            if (word.length() > letterCount){
+                letterCount = word.length();
+            }
+        }
+        for (String word: words) {
+            if (word.length() == letterCount) {
+                System.out.println(word);
+            }
+        }
     }
 }
+
